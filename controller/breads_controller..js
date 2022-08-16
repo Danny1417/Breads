c1b8800bb809b72b98f01359898d61ae237da3c8
Danny1417 <mr.mejia1417@gmail.com>
2022-08-15 20:54:42 -0400

    added jsx

--- controller/breads_controller..js
+++ controller/breads_controller..js
@@ -1,10 +1,16 @@
 // configuration
 const Router = require("express").Router();
-const Bread = require("../models/bread")
+const Bread = require("../models/bread");
+
 
 // index
 Router.get("/", (req, res) => {
-      res.send(Bread)
+      res.render(Index,
+            {
+                  breads: Bread,
+                  title: "Index Page"
+      }
+      )
 });
 // show
 Router.get("/:arrayIndex", (req, res) => {
